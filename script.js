86a24310bf994808f3fb77b43f4254337717fc16
Tihon3Face <121517033+Tihon3Face@users.noreply.github.com>
2023-03-15 11:41:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// var num = Number(prompt("Выбери число", ""));
+// do {
+//     var num = Number(prompt("Выбери число", ""));
+// } while (!num);
+// if (num < 10)
+// alert("Маловато");
+// else if (num < 100)
+// alert("Нормально");
+// else
+// alert("Многовато");
+
+var theNumber = Number(prompt("Введи Число", ""));
+if (!isNaN(theNumber))
+alert("и чего ты добился даун на какойто сайт зашёл блять лучше бы о матери подумал еблан тупое ");
+else(theNumber = 0)
+alert("Ну по-русски же сказал лучше маме помоги бездарь а не майнкрафт играй");
+
+
+// var number = 0;
+// while (number <= 12) {
+// console.log(number);
+// number = number + 2;
+// }
+
+// do {
+//     var name = prompt("Who are you?");
+//     } while (!name);
